6d438c5184c6243362a8d61f005159bace6444c7
Trubar4 <46722193+Trubar4@users.noreply.github.com>
2026-01-30 19:23:58 +0100

    Change base path for deployment in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '' // set to '/<repo-name>/' if deploying under subpath
+  base: '/rsvp-reader/' // set to '/<repo-name>/' if deploying under subpath
 });
