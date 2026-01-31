2c691ce801dc22694c6ecd721949204a3fd1011a
Philipp Thurnher <trubar4@gmail.com>
2026-01-31 16:12:44 +0100

    fix: correct script tag in index.html

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/rsvp-reader/' // set to '/<repo-name>/' if deploying under subpath
+  base: '' // Vercel (Root-Deploy) -> leer lassen
 });
